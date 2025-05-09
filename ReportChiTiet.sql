4781d38d5ccab77d0af3759132efcfcf29f9bcb0
VietNga00 <Nvvnga1103@gmail.com>
2025-05-09 14:48:03 +0700

    Add files via upload

---
+++ ReportChiTiet.sql
@@ -0,0 +1,33 @@
+USE TMS_Core_DEV
+GO
+
+
+CREATE PROCEDURE GH_DeNghi_ChiTiet
+	@denghiID INT
+	
+AS
+BEGIN	
+	SET NOCOUNT ON;  
+
+	SELECT 
+		GH_DeNghi.Id,
+		GH_DanhSachKhach.DeNghiId,
+		GH_DanhSachKhach.TenKhach,
+		GH_DanhSachKhach.ChucDanh,
+		GH_DanhSachKhach.TrangThai,
+		GH_DatPhong.DeNghiId AS GH_DatPhong_DeNghiId,
+		GH_DatPhong.KhachId,
+		GH_DatPhong.GhiChu,
+		GH_DatPhong.AllDay,
+		GH_DatPhong.TuNgay,
+		GH_DatPhong.DenNgay,
+		GH_Phong.MaPhong,
+		GH_Phong.TenPhong
+	FROM GH_DeNghi
+	INNER JOIN GH_DanhSachKhach ON GH_DanhSachKhach.DeNghiId = GH_DeNghi.Id
+	INNER JOIN GH_DatPhong ON GH_DatPhong.KhachId = GH_DanhSachKhach.Id AND GH_DatPhong.DeNghiId = GH_DeNghi.Id
+	INNER JOIN GH_Phong ON GH_Phong.Id = GH_DatPhong.PhongId
+	WHERE GH_DeNghi.Id = @denghiID
+
+END
+GO
---
+++ ReportNoiDung.sql
@@ -0,0 +1,23 @@
+USE TMS_Core_DEV;
+GO
+
+
+CREATE PROCEDURE GH_DeNghi_NoiDung
+	@denghiID INT
+	
+AS
+BEGIN	
+	SET NOCOUNT ON;  
+
+	SELECT 
+		GH_DeNghi.Id,
+		DonVi.TenDonVi,
+		GH_DeNghi.NoiDung,
+		GH_DeNghi.SoLuongKhach,
+		GH_DeNghi.NgayTao
+	FROM GH_DeNghi
+	INNER JOIN DonVi ON DonVi.ID = GH_DeNghi.DonViId
+	WHERE GH_DeNghi.Id = @denghiID
+
+END
+GO
